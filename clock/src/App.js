90ac7be7da2a8e07e932601d87f951932ef92938
vixcin <142383961+vixcin@users.noreply.github.com>
2023-09-04 09:10:14 +0000

    Initial Commit

---
+++ clock/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       hello Wolrds
+      </header>
+    </div>
+  );
+}
+
+export default App;
